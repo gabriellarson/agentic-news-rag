bdc67098cd27c0b91da2126b62f22d5fb1946347
Your Name <55459720+gabriellarson@users.noreply.github.com>
2025-07-23 16:13:57 -0500

    update setup_qdrant

--- scripts/setup_qdrant.py
+++ scripts/setup_qdrant.py
@@ -1,11 +1,14 @@
 from qdrant_client import QdrantClient, models
+import json
 
-client = QdrantClient(url="http://localhost:6333")
+config = json.load(open("config.json", 'r'))
+
+client = QdrantClient(url=config["qdrant_client_url"])
 
 collections_list = client.get_collections()
 
-if not any(c.name == "article-collection" for c in collections_list.collections):
-    client.create_collection(collection_name="article-collection", vectors_config=models.VectorParams(size=1024, distance=models.Distance.COSINE))
+if not any(c.name == config["qdrant_collection_name"] for c in collections_list.collections):
+    client.create_collection(collection_name=config["qdrant_collection_name"], vectors_config=models.VectorParams(size=1024, distance=models.Distance.COSINE))
     print("Created collection")
 else:
     print("Collection name already in use")
