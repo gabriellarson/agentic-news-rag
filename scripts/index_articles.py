18104d7ef858a7e2eda347a8ef5ea9a64467e957
Your Name <55459720+gabriellarson@users.noreply.github.com>
2025-07-23 15:35:14 -0500

    use config.json in index_articles

--- scripts/index_articles.py
+++ scripts/index_articles.py
@@ -1,15 +1,17 @@
 from qdrant_client import QdrantClient, models
 import uuid
 import hashlib
-import os
 from pathlib import Path
 import lmstudio as lms
+import json
 
-client = QdrantClient(url="http://localhost:6333")
+config = json.load(open("config.json", 'r'))
 
-embedding_model = lms.embedding_model("text-embedding-qwen3-embedding-0.6b")
+client = QdrantClient(url=config["qdrant_client_url"])
 
-folder = Path("../articles")
+embedding_model = lms.embedding_model(config["lmstudio_embedding"])
+
+folder = Path(config["articles_path"])
 txts = list(folder.glob("*.txt"))
 
 def parse(txt):
@@ -26,7 +28,7 @@ def parse(txt):
 for txt in txts:
 
     txt_id = str(uuid.UUID(hashlib.md5(txt.name.encode('utf-8')).hexdigest())) # use article name for deterministic UUID
-    if(len(client.retrieve(collection_name="article-collection", ids=[txt_id], with_payload=False, with_vectors=False)) > 0): # check if UUID exists already
+    if(len(client.retrieve(collection_name=config["qdrant_collection_name"], ids=[txt_id], with_payload=False, with_vectors=False)) > 0): # check if UUID exists already
         continue
 
     txt_payload = parse(txt)
@@ -34,7 +36,7 @@ for txt in txts:
     txt_vector = embedding_model.embed(f'{txt_payload["title"]}\n\n{txt_payload["subtitle"]}\n\n{txt_payload["content"]}')
 
     client.upsert(
-        collection_name="article-collection",
+        collection_name=config["qdrant_collection_name"],
         points=[
             models.PointStruct(
                 id = txt_id,
